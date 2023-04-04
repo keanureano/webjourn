0fc4fc36af318d628f8773fb3eb396c11fd0581b
Keanu Reaño <reanokeanu@gmail.com>
2023-04-04 12:55:03 +0800

    add post serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from api.models import Post
 
+
+# Serializers are used to convert User/Post model instances to JSON format for API responses.
 
 class UserSerializer(serializers.ModelSerializer):
-    # Include only the id and username fields of the User model
+    # Gets all user's posts
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    
+    # Fields to include in serialized data
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'posts']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    # Gets owner for each post
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    # Fields to include in serialized data
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'owner']
