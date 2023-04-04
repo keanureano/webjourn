80f3a0becf2e8bc2cdd9de43781c26485b95c8f4
Keanu Reaño <reanokeanu@gmail.com>
2023-04-04 13:52:47 +0800

    add post url paths

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
